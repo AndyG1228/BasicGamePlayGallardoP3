d8ad611f07b1d3275ec79896109367737ce3723d
AndyG1228 <606925@student.dusd.net>
2023-03-08 11:30:24 -0800

    Began Bonus Feature
    Enabled player movement on the z axis; trying to program aggressive animals.

--- BasicGamePlayGallardoP3/Assets/Scripts/PlayerController.cs
+++ BasicGamePlayGallardoP3/Assets/Scripts/PlayerController.cs
@@ -7,8 +7,11 @@ public class PlayerController : MonoBehaviour
     public GameObject projectilePrefab;
 
     public float horizontalInput;
-    public float speed = 15.0f;
-    public float xRange = 15;
+    public float verticalInput;
+    public float speed = 20.0f;
+    public float xRange = 24;
+    public float topRange = 16;
+    public float bottomRange = -1;
 
     // Start is called before the first frame update
     void Start()
@@ -32,7 +35,20 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
-        
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
+        //To keep the player withinn the game boundaries
+        if (transform.position.z < bottomRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, bottomRange);
+        }
+        if (transform.position.z > topRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y,topRange);
+        }
+
         //Launch a copied projectile from the player upon pressing spacebar
         if (Input.GetKeyDown(KeyCode.Space))
         {
